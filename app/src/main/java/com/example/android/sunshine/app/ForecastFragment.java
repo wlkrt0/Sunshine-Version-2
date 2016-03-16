7acdaeba7a4249d4157002c1ae06dbba62745b46
Zach Walker <zachwalker@zachwalker.net>
2016-03-15 20:33:02 -0700

    Completed lesson 2. Now parsing weather API response and displaying results in UI. 	modified:   app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,18 +14,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by zach on 3/14/2016.
  */
 public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> forecastAdapter;
 
     public ForecastFragment() {
     }
@@ -50,10 +60,10 @@ public class ForecastFragment extends Fragment {
             //perform a refresh of the weather data
             //call the networking code to get real weather data
             FetchWeatherTask weatherWorker = new FetchWeatherTask();
-            weatherWorker.execute();
+            weatherWorker.execute("43443");
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item); //defaults to false per javadoc, showing that we did not handle the menu item selection
     }
 
     @Override
@@ -73,7 +83,7 @@ public class ForecastFragment extends Fragment {
 
 
         //bind the mock weather forecast data above to the list view control
-        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+        forecastAdapter = new ArrayAdapter<String>(
                 this.getActivity(),
                 R.layout.list_item_layout,
                 R.id.list_item_forecast_textview,
@@ -85,11 +95,28 @@ public class ForecastFragment extends Fragment {
         //android needs every fragment.onCreateView(...) method to return inflated XML
         return rootView;
     }
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        private static final String OWeatherAPIkey = "473d04c5c8aa6d960b814744bf0b3ac7";
+        private static final String tempUnits = "metric";
+        private static final int forecastDays = 7;
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            //try to implement networking code here but expect failure.
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+            forecastAdapter.clear();
+            for (String dayForecast : strings) {
+                forecastAdapter.add(dayForecast);
+            }
+        }
+
+        @Override
+        protected String[] doInBackground(String... postcode) {
+
+            if (postcode.length == 0) {
+                return null;
+            }
+
             //android won't let you run networking code on the main UI thread.
             //Declare these two outside the try/catch so that they can be closed in the finally block
             HttpURLConnection urlConnection = null;
@@ -102,7 +129,19 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=43443&mode=JSON&units=metric&cnt=7&APPID=473d04c5c8aa6d960b814744bf0b3ac7");
+                Uri.Builder URIbuilder = new Uri.Builder();
+                URIbuilder.scheme("http")
+                        .authority("api.openweathermap.org")
+                        .appendPath("data")
+                        .appendPath("2.5")
+                        .appendPath("forecast")
+                        .appendPath("daily")
+                        .appendQueryParameter("q", postcode[0])
+                        .appendQueryParameter("mode", "JSON")
+                        .appendQueryParameter("units", tempUnits)
+                        .appendQueryParameter("cnt", Integer.toString(forecastDays))
+                        .appendQueryParameter("APPID", OWeatherAPIkey);
+                URL url = new URL(URIbuilder.build().toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -130,8 +169,8 @@ public class ForecastFragment extends Fragment {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                forecastJsonStr = buffer.toString(); //TODO: our JSON isn't being used right now
-                Log.v("ForecastFragment", forecastJsonStr);
+                forecastJsonStr = buffer.toString();
+                return this.extractForecastFromJSON(forecastJsonStr);
             } catch (IOException e) {
                 Log.e("ForecastFragment", "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
@@ -149,7 +188,47 @@ public class ForecastFragment extends Fragment {
                     }
                 }
             }
-            return null;
+            //return null; now unreachable
+        }
+
+        private String[] extractForecastFromJSON (String weatherJSON) {
+            try {
+                JSONObject weatherAPIresponse = new JSONObject(weatherJSON);
+                JSONArray weekForecast = weatherAPIresponse.getJSONArray("list");
+                String[] finalForecast = new String[weekForecast.length()];
+                for (int i = 0; i < weekForecast.length(); i++) {
+                    finalForecast[i] = this.getDayForecast(weekForecast, i);
+                }
+                return finalForecast;
+        } catch (JSONException exception) {
+                Log.e("ForecastFragment", "Error creating JSON object for weather API response");
+                throw new RuntimeException(exception);
+            }
+        }
+
+        private String getDayForecast(JSONArray weekForecast, int dayIndex) {
+            try {
+                JSONObject dayForecast = weekForecast.getJSONObject(dayIndex);
+                //determine the name of the day (eg. Tomorrow, Friday, etc)
+                //note that we must now use GregorianCalendar instead of Time which was deprecated in Android API level 22
+                GregorianCalendar calendar = new GregorianCalendar();
+                calendar.add(GregorianCalendar.DATE, dayIndex);
+                Date timeNow = calendar.getTime();
+                SimpleDateFormat todayDate = new SimpleDateFormat("EEE MMM dd");
+                String dayName = todayDate.format(timeNow);
+                //determine weather description text (eg. Sunny, Cloudy, etc)
+                String weatherDesc = dayForecast.getJSONArray("weather").getJSONObject(0).getString("main");
+                //get max and min temp. Note no unit conversion is required here because our API query specified our desired units
+                double maxTemp = dayForecast.getJSONObject("temp").getDouble("max");
+                double minTemp = dayForecast.getJSONObject("temp").getDouble("min");
+                String maxTempText = Integer.toString((int) Math.round(maxTemp));
+                String minTempText = Integer.toString((int) Math.round(minTemp));
+                String dayForecastText = dayName + " - " + weatherDesc + " - " + maxTempText + "/" + minTempText;
+                return dayForecastText;
+            } catch (JSONException exception) {
+                Log.e("ForecastFragment", "Error extracting daily forecast from weekly weather JSONArray");
+                throw new RuntimeException(exception);
+            }
         }
     }
 }
