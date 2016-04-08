d934f1ef0c3e811c945d115a2a4eea74f3019db5
Zach Walker <zachwalker@zachwalker.net>
2016-04-07 22:19:57 -0700

    Completed 7 of 48 steps for Lesson 5. Stylized the weekly weather forecast using a uniform style for each day.

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.sunshine.app.data.WeatherContract;
@@ -22,31 +23,31 @@ public class ForecastAdapter extends CursorAdapter {
     /**
      * Prepare the weather high/lows for presentation.
      */
-    private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
-        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
-        return highLowStr;
-    }
+//    private String formatHighLows(double high, double low) {
+//        boolean isMetric = Utility.isMetric(mContext);
+//        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
+//        return highLowStr;
+//    }
 
     /*
         This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
         string.
      */
-    private String convertCursorRowToUXFormat(Cursor cursor) {
-        // get row indices for our cursor
-        int idx_max_temp = ForecastFragment.COL_WEATHER_MAX_TEMP;
-        int idx_min_temp = ForecastFragment.COL_WEATHER_MIN_TEMP;
-        int idx_date = ForecastFragment.COL_WEATHER_DATE;
-        int idx_short_desc = ForecastFragment.COL_WEATHER_DESC;
-
-        String highAndLow = formatHighLows(
-                cursor.getDouble(idx_max_temp),
-                cursor.getDouble(idx_min_temp));
-
-        return Utility.formatDate(cursor.getLong(idx_date)) +
-                " - " + cursor.getString(idx_short_desc) +
-                " - " + highAndLow;
-    }
+//    private String convertCursorRowToUXFormat(Cursor cursor) {
+//        // get row indices for our cursor
+//        int idx_max_temp = ForecastFragment.COL_WEATHER_MAX_TEMP;
+//        int idx_min_temp = ForecastFragment.COL_WEATHER_MIN_TEMP;
+//        int idx_date = ForecastFragment.COL_WEATHER_DATE;
+//        int idx_short_desc = ForecastFragment.COL_WEATHER_DESC;
+//
+//        String highAndLow = formatHighLows(
+//                cursor.getDouble(idx_max_temp),
+//                cursor.getDouble(idx_min_temp));
+//
+//        return Utility.formatDate(cursor.getLong(idx_date)) +
+//                " - " + cursor.getString(idx_short_desc) +
+//                " - " + highAndLow;
+//    }
 
     /*
         Remember that these views are reused as needed.
@@ -54,7 +55,6 @@ public class ForecastAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
         return view;
     }
 
@@ -63,10 +63,33 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
+        //TODO resume here
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        //get all the textviews/imageviews that we will populate with weather data from our cursor
+        TextView tvHighTemp = (TextView) view.findViewById(R.id.list_item_high_textview);
+        TextView tvLowTemp = (TextView) view.findViewById(R.id.list_item_low_textview);
+        TextView tvDate = (TextView) view.findViewById(R.id.list_item_date_textview);
+        TextView tvDesc = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        ImageView ivIcon = (ImageView) view.findViewById(R.id.list_item_icon);
+
+        // get row indices for our cursor
+        int idx_max_temp = ForecastFragment.COL_WEATHER_MAX_TEMP;
+        int idx_min_temp = ForecastFragment.COL_WEATHER_MIN_TEMP;
+        int idx_date = ForecastFragment.COL_WEATHER_DATE;
+        int idx_short_desc = ForecastFragment.COL_WEATHER_DESC;
+
+        //use the cursor to build the strings to be displayed
+        boolean isMetric = Utility.isMetric(mContext);
+        String highTemp = Utility.formatTemperature(cursor.getDouble(idx_max_temp), isMetric);
+        String lowTemp = Utility.formatTemperature(cursor.getDouble(idx_min_temp), isMetric);
+        String wDate = Utility.getFriendlyDayString(context, cursor.getLong(idx_date));
+        String wDesc = cursor.getString(idx_short_desc);
+
+        //set the textviews to display our strings
+        tvHighTemp.setText(highTemp);
+        tvLowTemp.setText(lowTemp);
+        tvDate.setText(wDate);
+        tvDesc.setText(wDesc);
+        ivIcon.setImageResource(R.drawable.ic_launcher); //nothing for ivIcon yet - will just use a default image for now
     }
 }
